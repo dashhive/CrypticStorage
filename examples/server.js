d8c673c905602c9147c1734734b46ea352d4638d
jojobyte <184880+jojobyte@users.noreply.github.com>
2023-09-23 21:01:27 -0600

    test: :package: Update package.json and rename tests to examples

--- examples/server.js
+++ examples/server.js
@@ -1,10 +1,10 @@
-const CrypticStorage = require('../crypticstorage.js');
+const Cryptic = require('../cryptic.js');
 
 const STOREAGE_SALT = 'cloud robot resist squeeze';
 
 async function main() {
   const testMessage = 'what is the air speed velocity of an unlaiden swallow?';
-  const cw = CrypticStorage.encryptString('pass', STOREAGE_SALT);
+  const cw = Cryptic.encryptString('pass', STOREAGE_SALT);
   const iv = cw.getInitVector();
   const encrypted = await cw.encrypt(testMessage, iv);
   const decrypted = await cw.decrypt(encrypted, iv);
@@ -14,7 +14,7 @@ async function main() {
   console.log('Decrypted Test', [decrypted]);
 
   console.log('Copyable to Browser', [
-    `${encrypted}:${CrypticStorage.bufferToHex(iv)}`,
+    `${encrypted}:${Cryptic.bufferToHex(iv)}`,
   ]);
 
   let valFromBrowser =
@@ -28,10 +28,25 @@ async function main() {
 
   // let badIV = '2830ee00182b3113bab6427158c22677'
 
-  // console.log('Test from Browser with bad IV; should fail', [
-  //   badIV,
-  //   await cw.decrypt(data, badIV)
-  // ])
+  // try {
+  //   let wontDecrypt = await cw.decrypt(data, badIV)
+
+  //   console.log(
+  //     'Test from Browser with bad IV; this should not show',
+  //     [
+  //       badIV,
+  //       wontDecrypt,
+  //     ]
+  //   )
+  // } catch(err) {
+  //   console.error(
+  //     '\x1b[91m[failed] Test from Browser with bad IV\x1b[0m',
+  //     {
+  //       badIV,
+  //       err
+  //     }
+  //   )
+  // }
 }
 
 main();
